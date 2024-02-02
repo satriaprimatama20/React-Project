281b50a3cea00949a32b026730a03caa1ed11101
Satria Primatama <satriaprimatama20@gmail.com>
2024-02-02 11:34:35 +0700

    Versi 2

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import navigationbar from './components/navigationbar';
+function App() {
+  return (
+    <div>
+      <h>Testing</h>
+      <navigationbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navigationbar.js
@@ -0,0 +1,9 @@
+const navigationbar = () => {
+    return (
+        <div>
+            <h>Navigasi Testing</h>
+        </div>
+    )
+}
+
+export default navigationbar
